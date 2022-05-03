04a8a0f011b2b19d0b51c7dd7007683122e67eda
Abhishek Rawat <92290467+RawatXd@users.noreply.github.com>
2022-05-03 13:13:21 +0530

    Binary Search

---
+++ Binary Search/code_11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for(i=7; i >= 1; i-=2)
+    {
+        for(j=1; j<=i; j++)
+        {
+            cout<<j;
+        }
+  
+        cout<<endl;
+    }
+    return 0;
+}
